1fdda82233a201609fefa82a74bd530fe0de3a9d
abhinavramesh8 <abhinavramesh@hotmail.com>
2017-02-16 09:57:35 +0530

    Update newinterpreter.cpp

--- interpreter/newinterpreter.cpp
+++ interpreter/newinterpreter.cpp
@@ -15,7 +15,7 @@ class Token{
 public:
 	string type;
 	char value;
-	
+	int int_value;
 	Token()
 	{
 	type = NIL;
@@ -26,6 +26,7 @@ public:
 	
 		type= c.type;
 		value  = c.value;
+		int_value = c.int_value;
 	}
 	Token(string ty)
 	{
@@ -56,21 +57,31 @@ class Interpreter{
  	}
  	
  	Token get_next_token(){
+ 	
+ 		Token tk(NIL);
 		while(current_char){
-		Token tk(NIL);
-		text.get(current_char);
-		if(!current_char)
-		return tk;
-		
-		tk.value = current_char;		
-		if(current_char>= '0' && current_char<='9')
-			tk.type = INTEGER;
-		if(current_char=='+') tk.type = PLUS;
-		if(current_char=='-') tk.type = MINUS;
-		if(current_char=='/') tk.type = DIVIDE;
-		if(current_char=='*') tk.type = MULTIPLY;
-		return tk;
+		if(current_char==' ')
+		{
+			advance();
+			continue;
 		}
+		tk.value = current_char;
+		if(current_char>= '0' && current_char<='9')
+		{
+			tk.type = INTEGER,tk.int_value = integer();
+			return tk;
+		}
+			else
+		if(current_char=='+') tk.type = PLUS,advance();
+		else
+		if(current_char=='-') tk.type = MINUS,advance();
+		else
+		if(current_char == '/') tk.type = DIVIDE, advance();
+		else
+		if(current_char=='*') tk.type = MULTIPLY, advance();
+		return tk;			
+		}
+		return tk;
  	}
  	
  	void eat(string token_type)
@@ -95,41 +106,40 @@ class Interpreter{
  	{
  	int res = 0;
  	while(current_char && current_char>='0' && current_char<='9')
- 		res = res*10+(current_char-'0');
+ 	{	
+ 		res = res*10+(int)(current_char-'0');
+ 		advance();
+ 	}
  	return res;
  	}
  	
  	int calc(){
-	
-	Token left,right,op;
-	current_token = get_next_token();
-	left = current_token;
-	eat(INTEGER);
-	op = current_token;
-	eat(op.type);
-	right = current_token;
-	eat(INTEGER);
-	left.value = left.value-'0';
-	right.value = right.value - '0';
-	switch(op.type) {
-		case ADD: return (int)left.value+right.value;
-			  break;
 		
-		case MINUS: return (int)left.value-right.value;
-			    break;
-			
-		case MULTIPLY: return (int)left.value*right.value;
-			       break;
-			
-		case DIVIDE: return (int)left.value/right.value;
-			     break;
-	}
- 	
+		advance();
+		Token left,right,op;
+		current_token = get_next_token();
+		left = current_token;
+		eat(INTEGER);
+		op = current_token;
+		eat(op.type);
+		right = current_token;
+		eat(INTEGER);
+		
+		
+			if(op.type==PLUS)
+	 		return left.int_value+right.int_value;
+ 			if(op.type==MINUS)
+ 			return left.int_value-right.int_value;
+ 			if(op.type==MULTIPLY)
+ 			return left.int_value*right.int_value;
+ 			if(op.type==DIVIDE)
+ 			return left.int_value/right.int_value;
  	}
  	
  	};
-
-int main(int argc, char* argv[])
+ 	
+ 	
+ 	int main(int argc, char* argv[])
  	{
  		Interpreter in(argv[1]);
  		int result = in.calc();
