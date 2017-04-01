21bbedc811eff33e8e4e5d7de6d84d1aa6810696
abhinavramesh8 <abhinavramesh@hotmail.com>
2017-04-01 15:42:43 +0530

    Update parser.py

--- Interpreter/parser.py
+++ Interpreter/parser.py
@@ -1,409 +1,186 @@
-import ast
-from errors import TypescriptSyntaxError
-
-class ParserError(TypescriptSyntaxError):
-
-    def __init__(self, message, token):
-        super(ParserError, self).__init__(message, token.line, token.column)
-
-def enter_scope(parser, name):
-    class State(object):
-        def __enter__(self):
-            parser.scope.append(name)
-
-        def __exit__(self, exc_type, exc_val, exc_tb):
-            parser.scope.pop()
-
-    return State()
+import re
+from collections import namedtuple, OrderedDict
+from errors import TypescriptSyntaxError as LexerError
+from ttt import iteritems
 
 
-class Subparser(object):
+class Token(namedtuple('Token', ['name', 'value', 'line', 'column'])):          #token class for different types of tokens
 
-    PRECEDENCE = {
-        'call': 10,
-        'subscript': 10,
-        'unary': 9,
+    def __repr__(self):
+        return str(tuple(self))
 
-        '*': 7,
-        '/': 7,
-        '%': 7,
 
-        '+': 6,
-        '-': 6,
-
-        '>': 5,
-        '>=': 5,
-        '<': 5,
-        '<=': 5,
-
-        '==': 4,
-        '!=': 4,
-
-        '&&': 3,
-
-        '||': 2,
-
-        '..': 1,
-        '...': 1,
+def decode_str(s):
+    regex = re.compile(r'\\(r|n|t|\\|\'|")')                    
+    chars = {                                           #reular expressions for a string
+        'r': '\r',
+        'n': '\n',
+        't': '\t',
+        '\\': '\\',
+        '"': '"',
+        "'": "'",
     }
 
-    def get_subparser(self, token, subparsers, default=None):
-        cls = subparsers.get(token.name, default)
-        if cls is not None:
-            return cls()
-
-class PrefixSubparser(Subparser):
-
-    def parse(self, parser, tokens):
-        raise NotImplementedError()
-
-class InfixSubparser(Subparser):
-
-    def parse(self, parser, tokens, left):
-        raise NotImplementedError()
-
-    def get_precedence(self, token):
-        raise NotImplementedError()
-
-
-class NumberExpression(PrefixSubparser):
-
-    def parse(self, parser, tokens):
-        token = tokens.consume_expected('NUMBER')
-        return ast.Number(token.value)
-
-class StringExpression(PrefixSubparser):
-
-    def parse(self, parser, tokens):
-        token = tokens.consume_expected('STRING')
-        return ast.String(token.value)
-
-
-class NameExpression(PrefixSubparser):
-
-    def parse(self, parser, tokens):
-        token = tokens.consume_expected('NAME')
-        return ast.Identifier(token.value)
-
-
-class UnaryOperatorExpression(PrefixSubparser):
-
-    SUPPORTED_OPERATORS = ['-', '!']
-
-    def parse(self, parser, tokens):
-        token = tokens.consume_expected('OPERATOR')
-        if token.value not in self.SUPPORTED_OPERATORS:
-            raise ParserError('Unary operator {} is not supported'.format(token.value), token)
-        right = Expression().parse(parser, tokens, self.get_precedence(token))
-        if right is None:
-            raise ParserError('Expected expression'.format(token.value), tokens.consume())
-        return ast.UnaryOperator(token.value, right)
-
-    def get_precedence(self, token):
-        return self.PRECEDENCE['unary']
-
-
-class GroupExpression(PrefixSubparser):
-
-    def parse(self, parser, tokens):
-        tokens.consume_expected('LPAREN')
-        right = Expression().parse(parser, tokens)
-        tokens.consume_expected('RPAREN')
-        return right
-
-
-class ArrayExpression(PrefixSubparser):
-
-    def parse(self, parser, tokens):
-        tokens.consume_expected('LBRACK')
-        items = ListOfExpressions().parse(parser, tokens)
-        tokens.consume_expected('RBRACK')
-        return ast.Array(items)
-
-class BinaryOperatorExpression(InfixSubparser):
-
-    def parse(self, parser, tokens, left):
-        token = tokens.consume_expected('OPERATOR')
-        right = Expression().parse(parser, tokens, self.get_precedence(token))
-        if right is None:
-            raise ParserError('Expected expression'.format(token.value), tokens.consume())
-        return ast.BinaryOperator(token.value, left, right)
-
-    def get_precedence(self, token):
-        return self.PRECEDENCE[token.value]
-
-class CallExpression(InfixSubparser):
-
-    def parse(self, parser, tokens, left):
-        tokens.consume_expected('LPAREN')
-        arguments = ListOfExpressions().parse(parser, tokens)
-        tokens.consume_expected('RPAREN')
-        return ast.Call(left, arguments)
-
-    def get_precedence(self, token):
-        return self.PRECEDENCE['call']
-
-
-class SubscriptOperatorExpression(InfixSubparser):
-
-    def parse(self, parser, tokens, left):
-        tokens.consume_expected('LBRACK')
-        key = Expression().parse(parser, tokens)
-        if key is None:
-            raise ParserError('Subscript operator key is required', tokens.current())
-        tokens.consume_expected('RBRACK')
-        return ast.SubscriptOperator(left, key)
-
-    def get_precedence(self, token):
-        return self.PRECEDENCE['subscript']
-
-
-# expr: number_expr | str_expr | name_expr | group_expr | array_expr | dict_expr | prefix_expr | infix_expr | call_expr
-#     | subscript_expr
-class Expression(Subparser):
-
-    def get_prefix_subparser(self, token):
-        return self.get_subparser(token, {
-            'NUMBER': NumberExpression,
-            'STRING': StringExpression,
-            'NAME': NameExpression,
-            'LPAREN': GroupExpression,
-            'LBRACK': ArrayExpression,
-            'LCBRACK': DictionaryExpression,
-            'OPERATOR': UnaryOperatorExpression,
-        })
-
-    def get_infix_subparser(self, token):
-        return self.get_subparser(token, {
-            'OPERATOR': BinaryOperatorExpression,
-            'LPAREN': CallExpression,
-            'LBRACK': SubscriptOperatorExpression,
-        })
-
-    def get_next_precedence(self, tokens):
-        if not tokens.is_end():
-            token = tokens.current()
-            parser = self.get_infix_subparser(token)
-            if parser is not None:
-                return parser.get_precedence(token)
-        return 0
-
-    def parse(self, parser, tokens, precedence=0):
-        subparser = self.get_prefix_subparser(tokens.current())
-        if subparser is not None:
-            left = subparser.parse(parser, tokens)
-            if left is not None:
-                while precedence < self.get_next_precedence(tokens):
-                    op = self.get_infix_subparser(tokens.current()).parse(parser, tokens, left)
-                    if op is not None:
-                        left = op
-                return left
-
-
-class ListOfExpressions(Subparser):
-
-    def parse(self, parser, tokens):
-        items = []
-        while not tokens.is_end():
-            exp = Expression().parse(parser, tokens)
-            if exp is not None:
-                items.append(exp)
-            else:
-                break
-            if tokens.current().name == 'COMMA':
-                tokens.consume_expected('COMMA')
-            else:
-                break
-        return items
-
-
-class Block(Subparser):
-
-    def parse(self, parser, tokens):
-        tokens.consume_expected('LCBRACK')
-        statements = Statements().parse(parser, tokens)
-        tokens.consume_expected('RCBRACK')
-        return statements
-
-
-class FunctionStatement(Subparser):
-
-    def _parse_params(self, tokens):
-        params = []
-        if tokens.current().name == 'NAME':
-            while not tokens.is_end():
-                id_token = tokens.consume_expected('NAME')
-                params.append(id_token.value)
-                if tokens.current().name == 'COMMA':
-                    tokens.consume_expected('COMMA')
-                else:
-                    break
-        return params
-
-    def parse(self, parser, tokens):
-        tokens.consume_expected('FUNCTION')
-        id_token = tokens.consume_expected('NAME')
-        tokens.consume_expected('LPAREN')
-        arguments = self._parse_params(tokens)
-        tokens.consume_expected('RPAREN')
-        with enter_scope(parser, 'function'):
-            block = Block().parse(parser, tokens)
-        if block is None:
-            raise ParserError('Expected function body', tokens.current())
-        return ast.Function(id_token.value, arguments, block)
-
-
-class ConditionalStatement(Subparser):
-    def _parse_elif_conditions(self, parser, tokens):
-        conditions = []
-        while not tokens.is_end() and tokens.current().name == 'ELIF':
-            tokens.consume_expected('ELIF')
-            test = Expression().parse(parser, tokens)
-            if test is None:
-                raise ParserError('Expected `elif` condition', tokens.current())
-          #  tokens.consume_expected('COLON')
-            block = Block().parse(parser, tokens)
-            if block is None:
-                raise ParserError('Expected `elif` body', tokens.current())
-            conditions.append(ast.ConditionElif(test, block))
-        return conditions
-
-    def _parse_else(self, parser, tokens):
-        else_block = None
-        if not tokens.is_end() and tokens.current().name == 'ELSE':
-            #tokens.consume_expected('ELSE', 'COLON')
-            tokens.consume_expected('ELSE')
-            else_block = Block().parse(parser, tokens)
-            if else_block is None:
-                raise ParserError('Expected `else` body', tokens.current())
-        return else_block
-
-    def parse(self, parser, tokens):
-        tokens.consume_expected('IF')
-        test = Expression().parse(parser, tokens)
-        if test is None:
-            raise ParserError('Expected `if` condition', tokens.current())
-        #tokens.consume_expected('COLON')
-        if_block = Block().parse(parser, tokens)
-        if if_block is None:
-            raise ParserError('Expected if body', tokens.current())
-        elif_conditions = self._parse_elif_conditions(parser, tokens)
-        else_block = self._parse_else(parser, tokens)
-        return ast.Condition(test, if_block, elif_conditions, else_block)
-
-
-# loop_while_stmnt: WHILE expr COLON block
-class WhileLoopStatement(Subparser):
-
-    def parse(self, parser, tokens):
-        tokens.consume_expected('WHILE')
-        test = Expression().parse(parser, tokens)
-        if test is None:
-            raise ParserError('While condition expected', tokens.current())
-      #  tokens.consume_expected('COLON')
-        with enter_scope(parser, 'loop'):
-            block = Block().parse(parser, tokens)
-        if block is None:
-            raise ParserError('Expected loop body', tokens.current())
-        return ast.WhileLoop(test, block)
-
-
-# return_stmnt: RETURN expr?
-class ReturnStatement(Subparser):
-
-    def parse(self, parser, tokens):
-        if not parser.scope or 'function' not in parser.scope:
-            raise ParserError('Return outside of function', tokens.current())
-        tokens.consume_expected('RETURN')
-        value = Expression().parse(parser, tokens)
-        tokens.consume_expected('SEMICOLON')
-      #  tokens.consume_expected('NEWLINE')
-        return ast.Return(value)
-
-
-# break_stmnt: BREAK
-class BreakStatement(Subparser):
-
-    def parse(self, parser, tokens):
-        if not parser.scope or parser.scope[-1] != 'loop':
-            raise ParserError('Break outside of loop', tokens.current())
-        tokens.consume_expected('SEMICOLON')
-        return ast.Break()
-
-
-# cont_stmnt: CONTINUE
-class ContinueStatement(Subparser):
-
-    def parse(self, parser, tokens):
-        if not parser.scope or parser.scope[-1] != 'loop':
-            raise ParserError('Continue outside of loop', tokens.current())
-        tokens.consume_expected('SEMICOLON')
-        return ast.Continue()
-
-
-# assing_stmnt: expr ASSIGN expr NEWLINE
-class AssignmentStatement(Subparser):
-
-    def parse(self, parser, tokens, left):
-        tokens.consume_expected('ASSIGN')
-        right = Expression().parse(parser, tokens)
-       # tokens.consume_expected('NEWLINE')
-        tokens.consume_expected('SEMICOLON')
-        return ast.Assignment(left, right)
-
-
-# expr_stmnt: assing_stmnt
-#           | expr NEWLINE
-class ExpressionStatement(Subparser):
-
-    def parse(self, parser, tokens):
-        exp = Expression().parse(parser, tokens)
-        if exp is not None:
-            if tokens.current().name == 'ASSIGN':
-                return AssignmentStatement().parse(parser, tokens, exp)
-            else:
-               # tokens.consume_expected('NEWLINE')
-                tokens.consume_expected('SEMICOLON')
-                return exp
-
-
-# stmnts: stmnt*
-class Statements(Subparser):
-
-    def get_statement_subparser(self, token):
-        return self.get_subparser(token, {
-            'FUNCTION': FunctionStatement,
-            'IF': ConditionalStatement,
-            'WHILE': WhileLoopStatement,
-            'RETURN': ReturnStatement,
-            'BREAK': BreakStatement,
-            'CONTINUE': ContinueStatement,
-        }, ExpressionStatement)
-
-    def parse(self, parser, tokens):
-        statements = []
-        while not tokens.is_end():
-            statement = self.get_statement_subparser(tokens.current()).parse(parser, tokens)
-            if statement is not None:
-                statements.append(statement)
-            else:
-                break
-        return statements
-
-
-class Program(Subparser):
-
-    def parse(self, parser, tokens):
-        statements = Statements().parse(parser, tokens)
-        tokens.expect_end()
-        return ast.Program(statements)
-
-
-class Parser(object):
+    def replace(matches):
+        char = matches.group(1)[0]
+        if char not in chars:
+            raise Exception('Unknown escape character {}'.format(char))
+        return chars[char]
+
+    return regex.sub(replace, s[1:-1])
+
+
+def decode_num(s):                                # for decoding string
+    try:
+        return int(s)
+    except ValueError:
+        return float(s)
+
+
+class Lexer(object):                    #contains different types of tokens which can be present
+
+    rules = [
+        ('COMMENT', r'//.*'),
+        ('STRING', r'"(\\"|[^"])*"'),
+        ('STRING', r"'(\\'|[^'])*'"),
+        ('NUMBER', r'\d+\.\d+'),
+        ('NUMBER', r'\d+'),
+        ('NAME', r'[a-zA-Z_]\w*'),
+        ('WHITESPACE', '[ \t]+'),
+        ('NEWLINE', r'\n+'),
+        ('OPERATOR', r'[\+\*\-\/%]'),       # arithmetic operators
+        ('OPERATOR', r'<=|>=|==|!=|<|>'),   # comparison operators
+        ('OPERATOR', r'\|\||&&'),           # boolean operators
+        ('OPERATOR', r'\.\.\.|\.\.'),       # range operators
+        ('OPERATOR', '!'),                  # unary operator
+        ('ASSIGN', '='),
+        ('LPAREN', r'\('),
+        ('RPAREN', r'\)'),
+        ('LBRACK', r'\['),
+        ('RBRACK', r'\]'),
+        ('LCBRACK', '{'),
+        ('RCBRACK', '}'),
+        ('COLON', ':'),
+        ('COMMA', ','),
+        ('SEMICOLON',';'),
+    ]
+
+    keywords = {                         # different types of keywords present in the language
+        'function': 'FUNCTION',
+        'return': 'RETURN',
+        'else': 'ELSE',
+        'else if': 'ELIF',
+        'if': 'IF',
+        'while': 'WHILE',
+        'break': 'BREAK',
+        'continue': 'CONTINUE',
+        'when': 'WHEN',
+    }
+
+    ignore_tokens = [                     # whitespaces and newlines and comments are to be ignored
+        'WHITESPACE',
+        'COMMENT',
+        'NEWLINE',
+    ]
+
+    decoders = {											#tokens that are needed to be decoded
+        'STRING': decode_str,
+        'NUMBER': decode_num,
+    }
 
     def __init__(self):
-        self.scope = None
+        self.source_lines = []
+        self._regex = self._compile_rules(self.rules)
 
-    def parse(self, tokens):
-        self.scope = []
-        return Program().parse(self, tokens)
+    def _convert_rules(self, rules):
+        grouped_rules = OrderedDict()
+        for name, pattern in rules:
+            grouped_rules.setdefault(name, [])
+            grouped_rules[name].append(pattern)
+
+        for name, patterns in iteritems(grouped_rules):
+            joined_patterns = '|'.join(['({})'.format(p) for p in patterns])   #elements inside the bracket are joined by the '|'
+            yield '(?P<{}>{})'.format(name, joined_patterns)
+
+    def _compile_rules(self, rules):
+        return re.compile('|'.join(self._convert_rules(rules)))
+
+    def _tokenize_line(self, line, line_num):
+        pos = 0
+        while pos < len(line):
+            matches = self._regex.match(line, pos)
+            if matches is not None:
+                name = matches.lastgroup
+                pos = matches.end(name)
+                if name not in self.ignore_tokens:
+                    value = matches.group(name)
+                    if name in self.decoders:
+                        value = self.decoders[name](value)
+                    elif name == 'NAME' and value in self.keywords:
+                        name = self.keywords[value]
+                        value = None
+                    yield Token(name, value, line_num, matches.start() + 1)
+            else:
+                raise LexerError('Unexpected character {}'.format(line[pos]), line_num, pos + 1)
+
+    def _count_leading_characters(self, line, char):
+        count = 0
+        for c in line:
+            if c != char:
+                break
+            count += 1
+        return count
+
+
+    def tokenize(self, s):
+        tokens = []
+        line_num = 0
+        for line_num, line in enumerate(s.splitlines(), 1):
+            line = line.rstrip()
+
+            if not line:
+                self.source_lines.append('')
+                continue
+
+            self.source_lines.append(line)
+
+            line_tokens = list(self._tokenize_line(line, line_num))
+            if line_tokens:
+                tokens.extend(line_tokens)
+
+        return tokens
+
+
+class TokenStream(object):                  #tokens stream class which contain a  list of tokens
+
+    def __init__(self, tokens):
+        self._tokens = tokens
+        self._pos = 0
+
+    def consume_expected(self, *args):              # consumes the given type of token
+        token = None
+        for expected_name in args:
+            token = self.consume()
+            if token.name != expected_name:
+                raise LexerError('Expected {}, got {}'.format(expected_name, token.name), token.line, token.column)
+        return token
+
+    def consume(self):                      # consuming a particular token
+        token = self.current()
+        self._pos += 1
+        return token
+
+    def current(self):
+        try:
+            return self._tokens[self._pos]
+        except IndexError:
+            last_token = self._tokens[-1]
+            raise LexerError('Unexpected end of input', last_token.line, last_token.column)
+
+    def expect_end(self):
+        if not self.is_end():
+            token = self.current()
+            raise LexerError('End expected', token.line, token.column)
+
+    def is_end(self):
+        return self._pos == len(self._tokens)
